12ab0aa9e454458f1897185001f12d03188be776
Marin <marinmema@live.ca>
2025-02-14 11:37:21 -0500

    useRef, useState

--- src/hooks/useStopwatch.ts
+++ src/hooks/useStopwatch.ts
@@ -1,28 +1,37 @@
+import { useRef, useState } from "react";
+
 export type OnTick = () => void;
 
 export type Stopwatch = {
+    elapsed: number;
     start: (options: { onTick: OnTick }) => void;
     tap: (message: string) => void;
     stop: () => void;
 };
 
 export function useStopwatch(): Stopwatch {
-    let interval: null | number = null;
+    const intervalRef = useRef<null | number>(null);
+    const [elapsed, setElapsed] = useState<number>(0);
 
-    console.log("useStopwatch");
+    console.log(`useStopwatch interval=${intervalRef.current}`);
 
     return {
+        elapsed,
         start({ onTick }) {
             console.log("starting...");
-            if (interval !== null) {
+            if (intervalRef.current !== null) {
                 console.log("already started");
                 return;
             }
-            interval = window.setInterval(onTick, 1000);
+            setElapsed(0);
+            intervalRef.current = window.setInterval(() => {
+                onTick();
+                setElapsed((e) => e + 1000);
+            }, 1000);
             console.log("started");
         },
         tap(message: string) {
-            if (interval === null) {
+            if (intervalRef.current === null) {
                 console.log("not started");
                 return;
             }
@@ -30,12 +39,12 @@ export function useStopwatch(): Stopwatch {
         },
         stop() {
             console.log("stopping...");
-            if (interval === null) {
+            if (intervalRef.current === null) {
                 console.log("already stopped");
                 return;
             }
-            window.clearInterval(interval);
-            interval = null;
+            window.clearInterval(intervalRef.current);
+            intervalRef.current = null;
             console.log("stopped");
         },
     };
